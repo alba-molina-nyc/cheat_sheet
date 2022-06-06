f681d2a07604211d87e74ee2c6ebaf2fdf8df38d
Username <albavmolinanyc@gmail.com>
2022-06-06 08:55:57 -0400

    adds notes on accessing

---
+++ core_data_types/accessing.py
@@ -0,0 +1,10 @@
+my_numbers = (1,2,3,4,5)
+
+# negative indexing
+my_numbers[-1]
+
+my_numbers[2:5] #slices allows to obtain many elements from starting at index 2 all the way but not including index 5
+
+my_numbers[:3] # everything up to position 3
+my_numbers[3:] # everything from the position 3 inclusive up to the end
+my_numbers[1:7:2] # indicates how many elements to jump when iterating over the interval 
