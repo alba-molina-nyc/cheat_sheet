415150ef1ecb0708c144a4340ff4f118dc71b563
Username <albavmolinanyc@gmail.com>
2022-06-05 21:02:43 -0400

    first last works!

---
+++ python_workout/firstlast.py
@@ -0,0 +1,45 @@
+"""write a function firstlast that takes a sequence (string, list, tuple) and returns the first and last elements of that sequence.
+
+str = "abdefg"
+return ag
+
+lst = [1,2,3,4,5,6]
+return 1,6
+
+
+"""
+
+from pyparsing import stringStart
+
+
+def firstlast(items):
+    first = ''
+    last = ''
+    len_counter = 0
+
+    while items != None: 
+        for i,v in enumerate(items):
+            first = items[0]
+            last = items [- 1]
+            len_counter += 1
+        print("first", "==>",first, "last=>" ,last)
+
+        break
+    return first, last
+
+
+            
+
+firstlast('vickygarafola')
+firstlast([9,1,2,3,4])
+firstlast([(1,2),(24,25),('yes', 'hell yes')])
+
+
+# firstlast('12345')
+
+#   for item in items:
+#         print(item, 'appears @ =>', len_counter)
+#         len_counter += 1
+
+
+
